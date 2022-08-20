f34e2ef1baf9c8148a444ff36982977d70f08cf2
Amal Dhiaf <dhiafamal616@gmail.com>
2022-08-20 10:03:38 +0100

    add content to the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 function Contact() {
+  const contactInformations= {
+    email : 'dhiafamal616@gmail.com',
+    phoneNumber: '+21620322353',
+    adress : { 
+      city : ' Sfax',
+      coutry : 'Tunisia',
+      street : ' route tunis km 2',
+      postalCode : 3069
+    }
+  }
   return (
-    <div>Contact</div>
+    <div>
+      <p> <i class="bi bi-envelope"></i> {contactInformations.email} </p>
+      <p> <i class="bi bi-telephone"></i> { contactInformations.phoneNumber} </p>
+      <p>  <i class="bi bi-geo-alt"></i> { contactInformations.adress.street}; {contactInformations.adress.city}; { contactInformations.adress.coutry} </p>
+    </div>
   )
 }
 
