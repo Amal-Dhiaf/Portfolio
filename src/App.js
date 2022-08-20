3c0148ac87d54e6c9cc4bafde484a6f871c857e1
Amal Dhiaf <dhiafamal616@gmail.com>
2022-08-20 11:19:33 +0100

    add skill details page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 import Projects from './pages/Projects'
 import Skills from './pages/Skills'
 import Contact from './pages/Contact'
-import Home from './pages/Home' 
-
+import Home from './pages/Home'
+import SkillDetails from './pages/SkillDetails'
 
 function App() {
   return (
-      <BrowserRouter>
-        <Navbar />
-        <div className='container mt-5'>
-        <Route exact path='/' component={Home}/>
-        <Route path= '/Projects' component={Projects}/>
-        <Route path= '/Skills' component={Skills} />
-        <Route path= '/contact' component={Contact}/>
-        </div>
-        
-      </BrowserRouter>
-    
+    <BrowserRouter>
+      <Navbar />
+      <div className='container mt-5'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/Projects' component={Projects} />
+          <Route path='/skills/:id' component={SkillDetails} />
+          <Route path='/Skills' component={Skills} />
+          <Route path='/contact' component={Contact} />
+        </Switch>
+      </div>
+
+    </BrowserRouter>
+
   );
 }
 
