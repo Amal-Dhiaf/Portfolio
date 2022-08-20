7dc26325add072ccf2228961a8913390fd2f1ae5
Amal Dhiaf <dhiafamal616@gmail.com>
2022-08-20 11:58:23 +0100

    Add notFound page

--- src/pages/SkillDetails.js
+++ src/pages/SkillDetails.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect} from 'react'
-import {useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
 function SkillDetails() {
     const params = useParams()
@@ -11,23 +11,23 @@ function SkillDetails() {
             .then((response) => { setSkills(response.data) })
             .catch((error) => { console.log(error) })
     }, [])
-    useEffect(() => { 
+    useEffect(() => {
         const foundSkill = skills.find(item => item.id === params.id)
         console.log(foundSkill)
         setSkill(foundSkill)
 
     }, [skills])
-  return (
-    <div>
-        { skill ? (
-        <div style ={{textAlign: 'Center'}}>
-<h1>  { skill.name} </h1>
-<img src={skill.image} width="200px" alt={skill.name} />
-<p> {skill.description} </p>
+    return (
+        <div>
+            {skill ? (
+                <div style={{ textAlign: 'Center' }}>
+                    <h1>  {skill.name} </h1>
+                    <img src={skill.image} width="200px" alt={skill.name} />
+                    <p> {skill.description} </p>
+                </div>
+            ) : (<h2> Skill not found</h2>)}
         </div>
-        ) : (<h2> Skill not found</h2>) }
-    </div>
-  )
+    )
 }
 
 export default SkillDetails
